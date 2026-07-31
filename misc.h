2eeba1100cd5e6c8b28c29d2c4f2b874d4812144
Chris Nicolls <cnicolls@inmusicbrands.com>
2026-07-31 19:37:04 -0400

    Whitespace

--- misc.h
+++ misc.h
@@ -6,9 +6,9 @@
  #ifndef MISC_H_
  #define MISC_H_
  
- #ifdef __cplusplus
- extern "C" {
- #endif
+#ifdef __cplusplus
+extern "C" {
+#endif
  
 #ifndef NULL
 #define NULL 0
@@ -32,19 +32,19 @@
 
 #define MEMBER_OFFSET(type, member) (uint32_t) &(((type *) 0)->member)
 
- #define REVERSE_ENDIANNESS_U16(x)   \
-    (                                \
-        ( (x >>  4) & 0x00FF ) |     \
-        ( (x <<  4) & 0xFF00 )       \
-    )
+#define REVERSE_ENDIANNESS_U16(x)	\
+(                                	\
+	( (x >>  4) & 0x00FF ) |     	\
+	( (x <<  4) & 0xFF00 )       	\
+)
 
-#define REVERSE_ENDIANNESS_U32(x)    \
-    (                                \
-        ( (x >> 24) & 0x000000FF ) | \
-        ( (x >>  8) & 0x0000FF00 ) | \
-        ( (x <<  8) & 0x00FF0000 ) | \
-        ( (x << 24) & 0xFF000000 )   \
-    )
+#define REVERSE_ENDIANNESS_U32(x)	\
+(                                	\
+	( (x >> 24) & 0x000000FF ) | 	\
+	( (x >>  8) & 0x0000FF00 ) | 	\
+	( (x <<  8) & 0x00FF0000 ) | 	\
+	( (x << 24) & 0xFF000000 )   	\
+)
  
 #define BASE2_SCALE(value, src_width, dest_width) ((value) << ((dest_width) - (src_width)) | ((value) >> (src_width) - (((dest_width)) - ((src_width)))))
 
